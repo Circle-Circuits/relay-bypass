0ccbc7d6f8601f2f8d2fdfe5592a2b7b1eaa51a6
Hubert Fauconnier <h.fauconnier@gmail.com>
2021-10-29 19:28:02 +0200

    updated code to support both latching and none latching relays

--- main.c
+++ main.c
@@ -11,26 +11,70 @@
 // Include configuration bits
 #include "header.h"
 
-int switchState(int state) { 
-    if (state == 0) {
+int switchState(int state, int relayType) { 
+    if (relayType == 0) { // momentary relay, send continuous 5v
+       if (state == 0) {
         GP2 = 1;
         GP0 = 1;
         return 1;
-    } 
+        } 
     else if (state == 1) {
-        state = 0;
-        GP2 = 0;
-        GP0 = 0;
-        return 0;
+            state = 0;
+            GP2 = 0;
+            GP0 = 0;
+            return 0;
+        } 
+    }
+    else if (relayType == 1) { // latching relay, send a 5v pulse
+        if (state == 0) {
+            GP2 = 0;
+            GP5 = 0;
+            GP0 = 1;
+            __delay_ms(20);
+            GP0 = 0;
+            return 1;
+        } 
+        else if (state == 1) {
+            state = 0;
+            GP2 = 1;
+            GP0 = 0;
+            GP5 = 1;
+            __delay_ms(20);
+            GP5 = 0;
+            return 0;
+        } 
     }
 }
 
+void blink(int n) {
+    for (int i=0; i<n; i=i+1) {
+        GP2 = 0;
+        __delay_ms(100);
+        GP2 = 1;
+        __delay_ms(100); 
+        GP2 = 0;
+    }
+}
+
+int toggleMomentary(int momentary) {
+    if (momentary == 0) momentary = 1;
+    else momentary = 0;
+    eeprom_write(0, momentary);
+    blink(3);
+    __delay_ms(500);
+    return momentary;
+}
+
 void main(void) {
-    uint8_t state; // pedal is on or off
-    uint8_t pressed; // incremented when footswitch remains pressed
+    int state; // pedal is on or off
+    int pressed; // incremented when footswitch remains pressed
+    int momentary; // momentary trigger on / off
+    int relayType; // 0 is single side stable; 1 is single coil latching
     
     state = 0;
     pressed = 0;
+    momentary = (int)eeprom_read(0);
+    relayType = 1;
     
     ANSEL = 0; // no analog GPIO
     CMCON = 0x07; // comparator off 
@@ -38,27 +82,30 @@ void main(void) {
     OPTION_REG = 0b01010001; // GPIO pull-ups are enabled
     WPU = 0b0010010; // enable pull up on GPIO 01 and 04
     
-    TRISIO0 = 0; // pin 7 for LED
-    TRISIO1 = 1; // pin 6 for switch sensor
-    TRISIO2 = 0; // pin 5 for Relay trigger 
+    TRISIO0 = 0; // pin 7 for Relay+
+    TRISIO1 = 1; // pin 6 for Switch 
+    TRISIO2 = 0; // pin 5 for LED 
     TRISIO3 = 0; // pin 4 is unused
-    TRISIO4 = 1; // pin 3 for Momentary on/off 
-    TRISIO5 = 0; // pin 2 is unused
+    TRISIO4 = 1; // pin 3 is unused
+    TRISIO5 = 0; // pin 2 is for Relay-
     
     GPIO = 0; // all the GPIOs are in low state (0V) when starting
+    
+    // Hold switch down on startup to deactivate momentary switch feature. LED will blink.
+    if (GP1 == 0) momentary = toggleMomentary(momentary);
     
     while(1) {
         if (GP1 == 0) {
             if (pressed == 0) {
                 pressed = 1;
-                state = switchState(state);
+                state = switchState(state, relayType);
             } 
-            else if (pressed < 100 && GP4 == 0) {
+            else if (pressed < 100 && momentary == 1) {
                 ++pressed;
             }
         } else if (GP1 == 1) {
             if (pressed > 35) { // if it was momentary on, turn off
-                state = switchState(state);
+                state = switchState(state, relayType);
             }
             pressed = 0;
         }
